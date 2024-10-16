6ec3e9f0997324e0c62202c01d80f694fc2c8657
flo <furnari.florian@hotmail.fr>
2024-10-16 10:40:46 +0200

    first commit : debut du site

---
+++ Back/src/main/java/com/flo/Doggy/DoggyApplication.java
@@ -0,0 +1,13 @@
+package com.flo.Doggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggyApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/com/flo/Doggy/DoggyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flo.Doggy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
